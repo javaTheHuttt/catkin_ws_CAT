f968333ef66e0e4d7aaaaf7709448e174df7aa5e
JavaTheHuttt <humanX@supersein.de>
2021-10-26 17:27:42 +0200

    First draft of subscriber
    - currently not working

---
+++ src/assignment2_publisher_subscriber/subscriber.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# using the example of the slides 02-19
+
+import rospy
+
+# this does not work
+# TODO: find out which type to import
+from autominy_msgs import Speed
+
+def received_speed_callback(raw_msg):
+    # TODO: we probably need to process this to get the speed e.g. as a float
+    print(raw_msg)
+
+
+rospy.init_node("assignment2_speed_subscriber")
+# TODO: find out python speed data type
+rospy.Subscriber("/sensors/speed", Speed, received_speed_callback)
+
+# block until node shuts down
+rospy.spin()
