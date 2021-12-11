5650f0ffe47fc0a84e359fc14e38360871de5622
099111114097 <cora_glass1@web.de>
2021-12-11 12:42:32 +0100

    Finished Assignment 7

--- src/assignment7_obstacle_avoidance/obstacle_avoidance.py
+++ src/assignment7_obstacle_avoidance/obstacle_avoidance.py
@@ -8,7 +8,9 @@ from sensor_msgs.msg import LaserScan
 # import from parent directory
 # import sys
 # sys.path.append('../AutoMiny-exercises')
-# from simple_drive_control.srv import *
+from simple_drive_control.srv import *
+
+points_skipped = 0
 
 class ObstacleAvoidanceNode:
 
@@ -19,10 +21,16 @@ class ObstacleAvoidanceNode:
         # register subscribers
         rospy.Subscriber("/sensors/rplidar/scan", LaserScan, self.received_scan_callback)
 
-        # self.driving_maneuver_client = rospy.ServiceProxy("driving_maneuver", DrivingManeuver)
+        self.driving_maneuver_client = rospy.ServiceProxy("driving_maneuver", DrivingManeuver)
 
     # callback for lidar scan
     def received_scan_callback(self, scan):
+        global points_skipped
+        if points_skipped < 10:
+            points_skipped += 1
+            return
+        else:
+            points_skipped = 0
         # print("scan.range_min", scan.range_min)
         # print("scan.range_max", scan.range_max)
         # print("scan.angle_increment", scan.angle_increment)
@@ -39,12 +47,12 @@ class ObstacleAvoidanceNode:
         left_range = scan.ranges[(num_points - 1) // 4]
         
         print("front range", front_range)
-        # print("left_range", left_range)
-        # print("right_range",right_range)
+        print("left_range", left_range)
+        print("right_range",right_range)
         # print("back_range", back_range)
 
         # calculate steering direction if obstacle is < 0.5m in front of the car
-        if front_range < 0.5:
+        if front_range < 0.8:
             # compare points left and right around the front point
             increment = 10 # index increment (for 360 points 1 increment equals 1 degree)
             left_increment_range = 0
@@ -60,6 +68,7 @@ class ObstacleAvoidanceNode:
                 # TODO: increment should be relative to num_points, this works only with 360 points
                 if increment > (num_points // 2):
                     print("Help, idk where to go, human take over!")
+                    return
 
             print("left_increment_range", left_increment_range)
             print("right_increment_range", right_increment_range)
@@ -67,8 +76,26 @@ class ObstacleAvoidanceNode:
             action = "right"
             if left_increment_range > right_increment_range:
                 action = "left"
-            
-            print(action)
+            self.driving_maneuver_client.call(direction="forward", steering=action, distance=0.1)
+        # if obstacle appears on the side of the car move a bit to the opposite side
+        elif left_range < 0.1:
+            if right_range >= 0.3:
+                self.driving_maneuver_client.call(direction="forward", steering="right", distance=0.2)
+                self.driving_maneuver_client.call(direction="forward", steering="left", distance=0.1)
+            else:
+                print("Help, idk where to go, human take over!")
+                return
+        elif right_range < 0.1:
+            if left_range >= 0.3:
+                self.driving_maneuver_client.call(direction="forward", steering="left", distance=0.2)
+                self.driving_maneuver_client.call(direction="forward", steering="right", distance=0.1)
+            else:
+                print("Help, idk where to go, human take over!")
+                return
+        else:
+            # if there is no obstacle close continue driving straight
+            self.driving_maneuver_client.call(direction="forward", steering="straight", distance=0.1)
+           
 
 
 if __name__ == "__main__":
